e430869e6bc4ed37e2db581db8387a2a4c61022b
Steven Gordon <steven@SRG-MBP.local>
2015-05-31 19:39:52 -0700

    Data structure working and AMoment working

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,21 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['angularMoment'])
 
-.controller('TimerCtrl', function($scope) {})
+//.controller('TimerCtrl', function($scope) {})
+.controller('TimerCtrl', ['$scope', 'moment', '$interval', '$state', 'Routines', function($scope, moment, $interval, $state, Routines) {
 
-.controller('ReportCtrl', function($scope, Chats) {
+  var allData = Routines.all(); //get the sample data from the factory
+
+  var oneRoutine = allData["Routine1"]; //focus on the first (now only) routine, for now
+
+  $scope.routineTitle = oneRoutine.title; //store the title
+
+  $scope.steps = oneRoutine.steps; //store the steps array
+
+  $scope.currentTime = moment().format('h:mm:ss a');
+
+}])
+
+.controller('ReportCtrl', function($scope) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
